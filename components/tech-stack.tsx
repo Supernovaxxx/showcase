e391e513cd32e4884aed75eea78df2be8039add1
julianastahelin <julianastahelin@gmail.com>
2023-09-21 18:13:24 -0300

    refactor: use Set() to create list of skills/tech-stack

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -1,22 +1,15 @@
 import { CertificatesProps } from "@/types/certificates"
 
 export default function TechStack({ certificates }: CertificatesProps) {
-    const skills = certificates
+    const skills = [...new Set(certificates
         .map((item) => item.skills)
-        .flat()
-
-    const uniqueSkills = skills.reduce((accumulator: string[], currentValue) => {
-        if (!accumulator.includes(currentValue)) {
-            accumulator.push(currentValue)
-        }
-        return accumulator
-    }, [])
-
+        .flat())]
+    
     return (
         <section className='m-8 flex flex-col gap-4 justify-center items-center'>
             <h2 className='text-xl font-bold text-slate-800'>Tech stack</h2>
             <div className='flex gap-2 m-2'>
-                {uniqueSkills.map((item: string) =>
+                {skills.map((item: string) =>
                     <div className='p-4 rounded-xl text-slate-700 bg-slate-200 flex items-center hover:bg-slate-700 hover:text-white hover:font-light'>
                         <p className='font-extralight'>{item}</p>
                     </div>)}
