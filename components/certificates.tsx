b6a98aef0e6a2dcbf7d28f56d12ab993b19e5c80
julianastahelin <julianastahelin@gmail.com>
2023-09-25 15:28:14 -0300

    refactor: indentation and add key property to array items

--- components/certificates.tsx
+++ components/certificates.tsx
@@ -7,7 +7,11 @@ export default function Certificates({ certificates }: CertificatesProps) {
     <section className='my-4 mx-8 text-slate-800 flex flex-col gap-4 justify-center items-center w-4/5'>
       <h2 className='text-xl font-bold text-slate-800'>Certificates</h2>
       <div className='flex gap-y-4 flex-wrap justify-evenly'>
-        {certificates.map((item: CertificateType) => <Certificate certificate={item} />)}
+        {
+          certificates.map((item: CertificateType, index) => (
+            <Certificate certificate={item} key={index}/>
+          ))
+        }
       </div>
     </section>
   )
