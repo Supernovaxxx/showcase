1e0d70595a9180a0fcc5439a58a939a10b1cfa76
julianastahelin <julianastahelin@gmail.com>
2023-09-25 15:48:52 -0300

    feat: add margin to certificate

--- components/certificate.tsx
+++ components/certificate.tsx
@@ -14,7 +14,7 @@ export function Certificate({ certificate }: CertificateProps) {
     }
 
     return (
-        <div onClick={handleFlip} className={`transition-all duration-1000 preserve-3d ease-in-out cursor-pointer w-72 h-56 ${activeCard ? 'my-rotate-y-180' : ''}`}>
+        <div onClick={handleFlip} className={`transition-all duration-1000 preserve-3d ease-in-out cursor-pointer m-2 w-72 h-56 ${activeCard ? 'my-rotate-y-180' : ''}`}>
             <div className='relative backface-hidden p-4 rounded-xl bg-slate-700 text-white flex flex-col w-full h-full hover:bg-slate-500' >
                 <p className='font-extrabold'>{certificate.title}</p>
                 <p className='font-extralight'>{certificate.issuer.title}</p>
