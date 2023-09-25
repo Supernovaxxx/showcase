3ab8f9d6a98656398004856df6ba5fbe2c98961c
julianastahelin <julianastahelin@gmail.com>
2023-09-25 15:23:43 -0300

    feat: create toggleSkill function, get skill_list and pass both to Skills component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
-import { Certificate } from '@/types/certificates'
+'use client'
+import { CertificateType } from '@/types/certificates'
 import data from '@/data/certificates.json'
 import Certificates from '@/components/certificates'
 import Skills from '@/components/skills'
+import { useState } from 'react'
+import { getSkillList } from '@/lib/data-loader'
+import { Skill } from '@/types/skills'
 
 export default function Home() {
-  let certificates: Certificate[] = data
+  let certificates: CertificateType[] = data
+  const SKILL_LIST = getSkillList()
+
+  const [selectedSkills, setSelectedSkills] = useState<Skill[]>([])
+
+  function toggleSkill(skill: Skill): void {
+    if (selectedSkills.includes(skill)) {
+      setSelectedSkills(selectedSkills.filter((selected) => selected !== skill))
+    } else {
+      setSelectedSkills([...selectedSkills, skill])
+    }
+  }
 
   return (
     <div className='w-100 flex flex-col justify-center items-center'>
-        <Skills certificates={certificates} />
-        <Certificates certificates={certificates} />
+      <Skills skills={SKILL_LIST} toggleSkill={toggleSkill} />
+      <Certificates certificates={certificates} />
     </div>
   )
 }
