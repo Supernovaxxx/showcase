fd86b89bc652306649ab145736de9e1eb07a9d49
julianastahelin <julianastahelin@gmail.com>
2023-11-07 10:22:44 -0300

    feat: create theme provider and theme toggler

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { CertificationSection, ContactSection, LearnlogSection } from '@/components/site/sections'
 import { ReferencesTable } from '@/components/core/references/table'
+import { ThemeToggler } from '@/components/site/theme-toggler'
 
 
 export default function Home() {
 
   return (
     <main>
+      <ThemeToggler />
       <ReferencesTable />
       {/* <LearnlogSection /> */}
       <CertificationSection />
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
+interface ProvidersProps extends ThemeProviderProps {
+    children: React.ReactNode
+}
 
-export function Providers({children}: React.PropsWithChildren) {
+export function Providers({ children, ...props }: ProvidersProps) {
     const queryClient = new QueryClient()
 
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
+            <NextThemesProvider {...props}>
+                {children}
+            </NextThemesProvider>
         </QueryClientProvider>
     )
 }
