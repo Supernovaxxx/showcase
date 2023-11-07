2e76cad2d576e64ecccdeb4220427f501e64183b
julianastahelin <julianastahelin@gmail.com>
2023-11-07 10:22:44 -0300

    feat: create theme provider and theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { Providers } from '@/components/providers'
-
 import '@/app/globals.css'
+import { Providers } from '@/components/providers'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,11 +18,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+          <Providers
+            attribute="data-theme"
+            enableSystem
+            disableTransitionOnChange
+          >
+              {children}
+          </Providers>
       </body>
     </html>
   )
---
+++ components/site/theme-toggler.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+
+export function ThemeToggler() {
+    const [mounted, setMounted] = React.useState(false)
+    const { setTheme } = useTheme()
+
+    React.useEffect(() => {
+        setMounted(true)
+      }, [])
+    
+      if (!mounted) {
+        return null
+      }
+
+    return (
+        <div className='flex justify-end items-center gap-2 px-8 py-4'>
+            <Button onClick={() => setTheme("light")}>
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+            </Button>
+            <Button onClick={() => setTheme("dark")}>
+                <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+            </Button>
+        </div>
+    )
+}
