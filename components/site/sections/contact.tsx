0a065b89d345ebe2f24942ab1e137acf9735d0aa
julianastahelin <julianastahelin@gmail.com>
2023-11-07 16:31:11 -0300

    style: use single quotes as pattern

--- components/site/sections/contact.tsx
+++ components/site/sections/contact.tsx
@@ -1,10 +1,10 @@
-import ContactForm from "@/components/core/contact-form"
+import ContactForm from '@/components/core/contact-form'
 
 
 export function ContactSection() {
     
     return (
-        <div className="flex flex-col justify-center items-center gap-2 p-8 w-100">
+        <div className='flex flex-col justify-center items-center gap-2 p-8 w-100'>
             <h2 className='text-3xl font-bold text-foreground'>
                 Contact form
             </h2>
