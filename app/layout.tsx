19807586e25e50aa00ac5e17c01af57ea39cbbf4
julianastahelin <julianastahelin@gmail.com>
2023-11-07 10:22:44 -0300

    feat: create theme provider and theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import '@/app/globals.css'
 import { Providers } from '@/components/providers'
+
+import '@/app/globals.css'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,11 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
-          <Providers>
-              {children}
-          </Providers>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   )
