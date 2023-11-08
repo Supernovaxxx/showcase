9fd492be371ff44d9a194caa08d3699ca6ae1acb
julianastahelin <julianastahelin@gmail.com>
2023-11-08 11:54:46 -0300

    feat: change theme toggler button

--- components/site/theme-toggler.tsx
+++ components/site/theme-toggler.tsx
@@ -1,22 +1,22 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 
 export function ThemeToggler() {
-    const { setTheme } = useTheme()
+    const { theme, setTheme } = useTheme()
 
     return (
         <div className='flex justify-end items-center gap-2 px-8 py-4'>
-            <Button onClick={() => setTheme("light")}>
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-            </Button>
-            <Button onClick={() => setTheme("dark")}>
-                <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+            <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+                {theme === 'light'
+                    ? <Moon className='h-[1.2rem] w-[1.2rem]' />
+                    : <Sun className='h-[1.2rem] w-[1.2rem]' />
+                }
             </Button>
         </div>
     )
