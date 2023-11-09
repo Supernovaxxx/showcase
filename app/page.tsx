fdd7b2e797bfbb81886c660ff9c1caebca473f98
julianastahelin <julianastahelin@gmail.com>
2023-11-09 17:47:59 -0300

    feat: create page scroll-progress animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { CertificationSection, ContactSection, LearnlogSection } from '@/components/site/sections'
 import { ReferencesTable } from '@/components/core/references/table'
 import { ThemeToggler } from '@/components/site/theme-toggler'
+import { ScrollProgress } from '@/components/site/scroll-progress'
 
 
 export default function Home() {
 
   return (
     <main>
+      <ScrollProgress />
       <ThemeToggler />
       <ReferencesTable />
       {/* <LearnlogSection /> */}
