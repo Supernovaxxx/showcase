354fcaccb877e4c3387fc8aae800efee9fd76258
julianastahelin <julianastahelin@gmail.com>
2023-11-29 15:24:37 -0300

    feat: add curriculum content and config contentlayer

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,7 +3,7 @@ import { defineDocumentType, makeSource } from '@contentlayer/source-files'
 
 export const Certificate = defineDocumentType(() => ({
     name: 'Certificate',
-    filePathPattern: `**/*.md`,
+    filePathPattern: `certificates/**/*.md`,
     fields: {
         title: { type: 'string', required: true },
         date: { type: 'string', required: true },
@@ -17,4 +17,20 @@ export const Certificate = defineDocumentType(() => ({
     },
 }))
 
-export default makeSource({ contentDirPath: 'content/certificates', documentTypes: [Certificate] })
+export const Experience = defineDocumentType(() => ({
+    name: 'Experience',
+    filePathPattern: `curriculum/**/*.md`,
+    fields: {
+        role: { type: 'string', required: true },
+        employment_type: { type: 'string', required: true },
+        company: { type: 'string', required: true },
+        location: { type: 'string', required: true },
+        start_date: { type: 'string', required: true },
+        end_date: { type: 'string', required: true },
+        skills: { type: 'list', of: {type:'string'}, required: true },
+        description: { type: 'string', required: true },
+        impact: { type: 'string', required: false },
+    }
+}))
+
+export default makeSource({ contentDirPath: 'content', documentTypes: [Certificate, Experience] })
